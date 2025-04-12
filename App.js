4afa116ce4dba303d6b907ddfb7abe2fae560f51
Benito <47949106+ben4you2@users.noreply.github.com>
2025-04-12 19:44:32 -0400

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to the React Interface</h1>
+      <p>This is the new React-based interface migrated from the original HTML.</p>
+    </div>
+  );
+}
+
+export default App;
